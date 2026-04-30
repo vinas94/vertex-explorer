ee0f7e02ad1d3f234f7aeeac6c8747283efe9a8e
vinas94 <ep.edvinas@outlook.com>
2026-04-30 22:33:41 +0200

    sync

--- vertex_explorer/ui/formatters.py
+++ vertex_explorer/ui/formatters.py
@@ -23,7 +23,9 @@ def _run_dots(runs: list) -> Text:
 
 def _fmt_region(resource_name: str) -> str:
     _, project, _, region, _, resource_id = resource_name.split("/")
-    return region.replace("europe-", "")
+    if config.X:
+        return region.split("-", maxsplit=1)[1]
+    return region
 
 
 def _fmt_name(resource_name: str) -> str:
